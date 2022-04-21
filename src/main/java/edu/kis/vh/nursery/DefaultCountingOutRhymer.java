a4ec148ff0c540a59a625944772805e8525af894
235999 <235999@CARBON.CTI.P.LODZ.PL>
2022-04-21 11:33:59 +0200

    8. Zmiana atrybutów klas do otrzymania jak najmniejszej widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,25 +2,25 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int SIZE = 12;
-    public static final int ERR_EMPTY = -1;
-    public static final int ERR_FULL = SIZE - 1;
+    private static final int SIZE = 12;
+    private static final int ERR_EMPTY = -1;
+    private static final int ERR_FULL = SIZE - 1;
 
     private final int[] numbers = new int[SIZE];
 
 
-    public int total = ERR_EMPTY;
+    private int total = ERR_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public boolean callCheck() {
+    protected boolean callCheck() {
         return total == ERR_EMPTY;
     }
 
-    public boolean isFull() {
+    protected boolean isFull() {
         return total == ERR_FULL;
     }
 
